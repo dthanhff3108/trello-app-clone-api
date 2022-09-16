53e1a49370c8fdb721281cf5c485abe7ede3409b
dthanhff3108 <duythanhtizzer@gmail.com>
2022-09-17 02:03:59 +0700

    setup-db

--- src/server.js
+++ src/server.js
@@ -1,19 +1,34 @@
 import express from 'express'
-import { connectDB } from './config/mongodb'
+import { connectDB, getDB } from './config/mongodb'
 import { env } from '*/config/environment.js'
-
-const app = express()
-
-const hostname = 'localhost'
-const port = 8000
+import { BoardModel } from '*/models/board.model'
 
 connectDB()
+    .then(() => console.log("Connected"))
+    .then(() => bootServer())
+    .catch(err=>{
+        console.log(err)
+        process.exit()
+    })
 
-app.get('/',(req,res)=>{
-    res.end('<h1>Hello TIzz</h1>')
-})
 
-app.listen(env.PORT,env.HOST,()=>{
-    console.log("hello duy thanh");
-})
+
+const bootServer = () => {
+    const app = express()
+
+    app.get('/test', async (req,res)=>{
+        const dataSample = {
+            title : "hello",
+            columnOrder : [],
+        }
+        let newBoard = await BoardModel.createNew(dataSample)
+        console.log(newBoard);
+        res.end('<h1>Hello TIzz</h1>')
+    })
+    
+    app.listen(env.APP_PORT,env.APP_HOST, () => {
+        console.log("hello duy thanh");
+    })
+}
+
 
